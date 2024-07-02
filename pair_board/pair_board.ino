7b4f24f237f7866957f8c5cf2474a47bd70585c0
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-07-02 14:26:10 +0200

    mise en forme + functions

--- pair_board/pair_board.ino
+++ pair_board/pair_board.ino
@@ -2,100 +2,131 @@
 
 SoftwareSerial master(11, 12);
 SoftwareSerial slave(9, 10);
+const int alimPin = 13;
 
-bool speak_with_master = true;
 String receiveBuffer = "";
 bool activeInteractiveMode = true;
+bool _speaking_with_master = true;
+bool _activeAlim = false;
+
+void alimON() {
+  _activeAlim = true;
+  digitalWrite(alimPin, LOW);
+}
+
+void alimOFF() {
+  _activeAlim = false;
+  digitalWrite(alimPin, HIGH);
+}
+
+bool alim() {
+  return _activeAlim;
+}
+
+void sendToMaster(String message) {
+  if (!_speaking_with_master) {
+    master.listen();
+    _speaking_with_master = true;
+  }
+  master.print(message + "\r\n");
+}
+
+void sendToSlave(String message) {
+  if (_speaking_with_master) {
+    slave.listen();
+    _speaking_with_master = false;
+  }
+  slave.print(message + "\r\n");
+}
+
+String _readFrom(SoftwareSerial *ser) {
+  String receive = "";
+  while (ser->available()) {
+    char receiveChar = ser->read();
+    if (receiveChar == '\r' || receiveChar == '\n') {
+      if (receiveChar == '\r') ser->read();  // Lire le '\n' suivant
+      break;
+    } else {
+      receive += receiveChar;
+    }
+  }
+  return receive;
+}
+
+String readFromMaster() {
+  return _readFrom(&master);
+}
+
+String readFromSlave() {
+  return _readFrom(&slave);
+}
 
 void setup() {
-    Serial.begin(38400);
-    Serial.println("configuring...");
-    master.begin(38400);
-    slave.begin(38400);
-    pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
-    Serial.println("speaking with master...");
-    master.listen();
+  Serial.begin(38400);
+  Serial.println("configuring...");
+  master.begin(38400);
+  slave.begin(38400);
+  pinMode(13, OUTPUT);
+  alimOFF();
+  Serial.println("speaking with master...");
+  master.listen();
 }
 
 void loop() {
-    transmitInfos();
-    if (activeInteractiveMode) {
-        interactiveMode();
-    }
+  transmitInfos();
+  if (activeInteractiveMode) {
+    interactiveMode();
+  }
 }
 
 void transmitInfos() {
-    // Lire depuis le master
-    if (master.available()) {
-        String receive = "";
-        while (master.available()) {
-            char receiveChar = master.read();
-            if (receiveChar == '\r' || receiveChar == '\n') {
-                if (receiveChar == '\r') master.read(); // Lire le '\n' suivant
-                break;
-            } else {
-                receive += receiveChar;
-            }
-        }
-        Serial.println("master : " + receive);
-    }
+  if (master.available()) {
+    Serial.println("master : " + readFromMaster());
+  }
 
-    // Lire depuis le slave
-    if (slave.available()) {
-        String receive = "";
-        while (slave.available()) {
-            char receiveChar = slave.read();
-            if (receiveChar == '\r' || receiveChar == '\n') {
-                if (receiveChar == '\r') slave.read(); // Lire le '\n' suivant
-                break;
-            } else {
-                receive += receiveChar;
-            }
-        }
-        Serial.println("slave  : " + receive);
-    }
+  if (slave.available()) {
+    Serial.println("slave  : " + readFromSlave());
+  }
 }
 
-
 void interactiveMode() {
-    // Lire depuis Serial
-    if (Serial.available()) {
-        while (Serial.available()) {
-            receiveBuffer += (char)Serial.read();
-            if (receiveBuffer.endsWith("\r\n")) {
-                processSerialCommand(receiveBuffer);
-                receiveBuffer = ""; // Réinitialiser le buffer après traitement
-            }
-        }
+  // Lire depuis Serial
+  if (Serial.available()) {
+    while (Serial.available()) {
+      receiveBuffer += (char)Serial.read();
+      if (receiveBuffer.endsWith("\r\n")) {
+        processSerialCommand(receiveBuffer);
+        receiveBuffer = "";  // Réinitialiser le buffer après traitement
+      }
     }
+  }
 }
 
 void processSerialCommand(String command) {
-    command.trim(); // Supprimer les espaces en début et en fin de la commande
-    if (command == "BC+WHICH?" || command == "bw") {
-        if (speak_with_master) {
-            Serial.println("board  : speaking with master");
-        } else {
-            Serial.println("board  : speaking with slave");
-        }
-    } else if (command == "BC+WHICH=MASTER" || command == "bwm") {
-        speak_with_master = true;
-        master.listen();
-        Serial.println("board  : now speaking with master");
-    } else if (command == "BC+WHICH=SLAVE" || command == "bws") {
-        speak_with_master = false;
-        slave.listen();
-        Serial.println("board  : now speaking with slave");
+  command.trim();  // Supprimer les espaces en début et en fin de la commande
+  if (command == "BC+WHICH?" || command == "bw") {
+    if (_speaking_with_master) {
+      Serial.println("board  : speaking with master");
     } else {
-        if (!command.startsWith("AT")) {
-            Serial.println("board  : unknown command : '" + command + "'");
-        } else {
-            if (speak_with_master) {
-                master.print(command + "\r\n");
-            } else {
-                slave.print(command + "\r\n");
-            }
-        }
+      Serial.println("board  : speaking with slave");
     }
+  } else if (command == "BC+WHICH=MASTER" || command == "bwm") {
+    _speaking_with_master = true;
+    master.listen();
+    Serial.println("board  : now speaking with master");
+  } else if (command == "BC+WHICH=SLAVE" || command == "bws") {
+    _speaking_with_master = false;
+    slave.listen();
+    Serial.println("board  : now speaking with slave");
+  } else {
+    if (!command.startsWith("AT")) {
+      Serial.println("board  : unknown command : '" + command + "'");
+    } else {
+      if (_speaking_with_master) {
+        sendToMaster(command);
+      } else {
+        sendToSlave(command);
+      }
+    }
+  }
 }
