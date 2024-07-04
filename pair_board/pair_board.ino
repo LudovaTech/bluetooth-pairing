0c78b78679cdc5da9dcb36f1f8bbfbeaeff37790
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-07-04 11:56:06 +0200

    sécurisation : getAdress pour AT+BIND et AT+PSWD

--- pair_board/pair_board.ino
+++ pair_board/pair_board.ino
@@ -111,16 +111,21 @@ String readFromSerial() {
 const String _pair1Commands[] = {
     "AT",
     "AT+ORGL",
+    "AT", //La commande après AT+ORGL échoue systématiquement, permet de passer la commande sans causer de problèmes
     "AT+ROLE=1",
     "AT+RESET",
+    "AT",
     "AT+RMAAD",
-    "AT+NAME=LUDOMASTER"};
+    "AT+NAME=LUDOMASTER",
+    "AT+PSWD=VATECH"};
 
 const String _pair2Commands[] = {
     "AT",
     "AT+ORGL",
+    "AT",
     "AT+RMAAD",
-    "AT+NAME=LUDOSLAVE"};
+    "AT+NAME=LUDOSLAVE",
+    "AT+PSWD=VATECH"};
 
 bool waitForOK(String *failed, String command, bool waitMaster) {
   while (true) {
@@ -154,6 +159,48 @@ bool waitForOK(String *failed, String command, bool waitMaster) {
   return false;
 }
 
+String getAdress(bool fromMaster) {
+  if (fromMaster) {
+    sendToMaster("AT+ADDR?");
+  } else {
+    sendToSlave("AT+ADDR?");
+  }
+  String message_address;
+  while (message_address.substring(0, 6) != "+ADDR:") {
+    if (fromMaster ? master.available() : slave.available()) {
+      if (fromMaster) {
+        message_address = readFromMaster();
+      } else {
+        message_address = readFromSlave();
+      }
+      message_address.trim();
+      if (message_address == "ERROR:[0]") {
+        Serial.println("FAILED : AT+ADDR");
+        return "";
+      }
+    }
+    if (Serial.available()) {
+      String message = readFromSerial();
+      message.trim();
+      if (message == "q") {
+        return "";
+      }
+    }
+  }
+  String address = message_address.substring(6);
+  address.replace(":", ",");
+  if (fromMaster) { // trap OK
+    if (master.available()) {
+      readFromMaster();
+    }
+  } else {
+    if (slave.available()) {
+      readFromSlave();
+    }
+  }
+  return address;
+}
+
 void pair() {
   String failed = "";
   Serial.println("board  : PAIR");
@@ -165,35 +212,16 @@ void pair() {
     sendToSlave(command);
     if (waitForOK(&failed, command, false)) return;
   }
-  sendToSlave("AT+ADDR?");
-  String message_address;
-  while (message_address.substring(0, 6) != "+ADDR:") {
-    if (slave.available()) {
-      message_address = readFromSlave();
-      message_address.trim();
-      if (message_address == "ERROR:[0]") {
-        Serial.println("FAILED : AT+ADDR");
-        return;
-      }
-    }
-    if (Serial.available()) {
-      String message = readFromSerial();
-      message.trim();
-      if (message == "q") {
-        return;
-      }
-    }
-  }
-  String address = message_address.substring(6);
-  address.replace(":", ",");
-  if (slave.available()) {
-    readFromSlave();  // trap OK
-  }
-  sendToMaster("AT+PAIR=" + address + ",20");
+  String addressSlave = getAdress(false);
+  String addressMaster = getAdress(true);
+  if (addressSlave == "") return;
+  if (addressMaster == "") return;
+  sendToMaster("AT+PAIR=" + addressSlave + ",20");
+  sendToSlave("AT+BIND=" + addressMaster);
   // AT+PAIR don't send OK I don't know why
-  sendToMaster("AT+BIND=" + address);
+  sendToMaster("AT+BIND=" + addressSlave);
   if (waitForOK(&failed, "AT+BIND", true)) return;
-  sendToMaster("AT+LINK=" + address);
+  sendToMaster("AT+LINK=" + addressSlave);
   if (waitForOK(&failed, "AT+LINK", true)) return;
 
   if (failed != "") {
